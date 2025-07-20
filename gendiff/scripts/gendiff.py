996f7dd1a3d2ca121427429ec82c524d1074c183
Viktor Arkhipov <arkhipov.vd@gmail.com>
2025-07-20 15:00:34 +0300

    gendiff/scripts/gendiff.py has been edited

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,10 @@
+import json
 import argparse
 
+
+def open_json(filepath):
+    with open(filepath) as f:
+        return json.load(f)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -7,11 +12,17 @@ def main():
         files and shows a difference.'''
     )
 
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', help='set format of output')
 
-    parser.parse_args()
+    args = parser.parse_args()
+
+    print(
+        open_json(args.first_file),
+        open_json(args.second_file),
+        sep='\n'
+    )
 
 
 if __name__ == '__main__':
