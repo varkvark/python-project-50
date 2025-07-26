7213dce2ebd73449e2ef451c2f32ec2713def9f4
Viktor Arkhipov <arkhipov.vd@gmail.com>
2025-07-27 00:26:47 +0300

    gendiff/modules.py has been edited

--- gendiff/modules.py
+++ gendiff/modules.py
@@ -5,12 +5,14 @@ def open_json(filepath):
     with open(filepath) as f:
         return json.load(f)
 
+
 def sorted_dict(new_dict):
     result = dict(
         sorted(new_dict.items(), key=lambda key: key[0][4])
     )
 
     return result
+
 
 def dict_to_str(new_dict):
     result = '{\n'
@@ -20,20 +22,25 @@ def dict_to_str(new_dict):
     result += '}'
     return result
 
-def generate_diff(first_dict, second_dict):
+
+def generate_diff(first_file, second_file):
     result = {}
-    all_keys = set(first_dict) | set(second_dict)
+
+    first_json = open_json(first_file)
+    second_json = open_json(second_file)
+
+    all_keys = set(first_json) | set(second_json)
 
     for key in all_keys:
-        value_in_first = first_dict.get(key)
-        value_in_second = second_dict.get(key)
+        value_in_first = first_json.get(key)
+        value_in_second = second_json.get(key)
 
         if value_in_first == value_in_second:
             result[f'    {key}'] = value_in_first
         else:
-            if key in first_dict:
+            if key in first_json:
                 result[f'  - {key}'] = value_in_first
-            if key in second_dict:
+            if key in second_json:
                 result[f'  + {key}'] = value_in_second
 
     result = sorted_dict(result)
