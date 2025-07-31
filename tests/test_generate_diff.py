5473937e5d8879f251d79cf74e43fceb1f2bbf17
Viktor Arkhipov <arkhipov.vd@gmail.com>
2025-07-31 22:51:52 +0300

    Add new tests to tests/test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -18,3 +18,11 @@ def test_generate_diff_flat_extdiff():
     assert generate_diff('data/file1.yml', 'data/file2.yaml') == result
     assert generate_diff('data/file1.yml', 'data/file2.json') == result
     assert generate_diff('data/file1.json', 'data/file2.yaml') == result
+
+
+def test_generate_diff_recursive():
+    with open('tests/test_data/result.txt') as f:
+        result = f.read()[:-1]
+    assert generate_diff('data/file3.json', 'data/file4.json') == result
+    assert generate_diff('data/file3.yml', 'data/file4.yml') == result
+    assert generate_diff('data/file3.json', 'data/file4.yml') == result
