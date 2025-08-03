b1b2fb6800d38c6ec6128fff07d27495859033a3
Viktor Arkhipov <arkhipov.vd@gmail.com>
2025-08-03 20:55:09 +0300

    Add new tests to tests/test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,7 +2,7 @@ from gendiff.modules import generate_diff
 
 
 def test_generate_diff_flat():
-    with open('tests/test_data/correct_flat.txt') as f:
+    with open('tests/test_data/result_flat.txt') as f:
         result = f.read()[:-1]
     assert generate_diff('data/file1.json', 'data/file2.json') == result
     assert generate_diff('data/file1.yml', 'data/file2.yml') == result
@@ -11,8 +11,8 @@ def test_generate_diff_flat():
     assert generate_diff('data/file1.yml', 'data/file2.yaml') == result
 
 
-def test_generate_diff_flat_extdiff():
-    with open('tests/test_data/correct_flat.txt') as f:
+def test_generate_diff_flat_expansions():
+    with open('tests/test_data/result_flat.txt') as f:
         result = f.read()[:-1]
     assert generate_diff('data/file1.yaml', 'data/file2.yml') == result
     assert generate_diff('data/file1.yml', 'data/file2.yaml') == result
@@ -20,9 +20,25 @@ def test_generate_diff_flat_extdiff():
     assert generate_diff('data/file1.json', 'data/file2.yaml') == result
 
 
-def test_generate_diff_recursive():
-    with open('tests/test_data/result.txt') as f:
+def test_generate_diff_stylish():
+    with open('tests/test_data/result_stylish.txt') as f:
         result = f.read()[:-1]
     assert generate_diff('data/file3.json', 'data/file4.json') == result
     assert generate_diff('data/file3.yml', 'data/file4.yml') == result
     assert generate_diff('data/file3.json', 'data/file4.yml') == result
+
+
+def test_generate_diff_plain():
+    with open('tests/test_data/result_plain.txt') as f:
+        result = f.read()[:-1]
+    assert generate_diff('data/file3.json', 'data/file4.json', 'plain') == result  # noqa: E501
+    assert generate_diff('data/file3.yml', 'data/file4.yml', 'plain') == result
+    assert generate_diff('data/file3.json', 'data/file4.yml', 'plain') == result
+
+
+def test_generate_diff_json():
+    with open('tests/test_data/result_json.txt') as f:
+        result = f.read()[:-1]
+    assert generate_diff('data/file3.json', 'data/file4.json', 'json') == result
+    assert generate_diff('data/file3.yml', 'data/file4.yml', 'json') == result
+    assert generate_diff('data/file3.json', 'data/file4.yml', 'json') == result
