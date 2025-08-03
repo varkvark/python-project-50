ff7c35640dd7d028af884ec56136dd0adecb2304
Viktor Arkhipov <arkhipov.vd@gmail.com>
2025-08-03 20:57:35 +0300

    Entrance point gendiff/scripts/gendiff.py has been updated

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -11,7 +11,11 @@ def start_gendiff_cli():
 
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument(
+        '-f', '--format',
+        help='set format of output',
+        default='stylish'
+    )
 
     return parser.parse_args()
 
@@ -21,9 +25,10 @@ def main():
 
     first_file = args.first_file
     second_file = args.second_file
+    format_name = args.format
 
-    x = generate_diff(first_file, second_file)
-    print(x)
+    diff = generate_diff(first_file, second_file, format_name)
+    print(diff)
 
 
 if __name__ == '__main__':
